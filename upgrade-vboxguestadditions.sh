782e4d870d63c9dcc47183736bca18f6df6944e6
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-04-17 00:41:24 +0900

    VBoxGuestAdditions 업그레이드 시 오류를 바로잡고 청소합니다.

--- upgrade-vboxguestadditions.sh
+++ upgrade-vboxguestadditions.sh
@@ -3,5 +3,6 @@
 echo "[virtualbox] upgrade VBoxGuestAdditions"
 apt-get -y -qq install linux-headers-4.4.0-177-generic dkms
 curl -LO http://download.virtualbox.org/virtualbox/6.1.6/VBoxGuestAdditions_6.1.6.iso
-mkdir /mnt/iso && mount -o loop /home/vagrant/VBoxGuestAdditions_6.1.6.iso /mnt/iso
+mkdir -p /mnt/iso && mount -o loop /root/VBoxGuestAdditions_6.1.6.iso /mnt/iso
 yes | sh /mnt/iso/VBoxLinuxAdditions.run --nox11
+umount /mnt/iso && rm /root/VBoxGuestAdditions_6.1.6.iso
